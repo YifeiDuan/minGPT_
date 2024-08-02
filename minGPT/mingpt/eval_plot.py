ed22734a2109eaf20ac7f3598c5c9d6b1474a997
Yifei Duan <duanyf99@mit.edu>
2024-08-02 10:31:42 -0400

    eval comparison

--- minGPT/mingpt/eval_plot.py
+++ minGPT/mingpt/eval_plot.py
@@ -12,22 +12,16 @@ if __name__ == '__main__':
     ########## 0. Configurate the session ##########
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_type', default="gpt-mini") 
-    parser.add_argument('--ckpt_start', type=int, default=10)  
-    parser.add_argument('--ckpt_end', type=int, default=100) 
-    parser.add_argument('--ckpt_step', type=int, default=10) 
     parser.add_argument('--external_rep_modes',type=int, nargs='+', help='List of modes')
 
     args = parser.parse_args()
     model_type = args.model_type
-    ckpt_start = args.ckpt_start
-    ckpt_end = args.ckpt_end
-    ckpt_step = args.ckpt_step
     external_rep_modes = args.external_rep_modes
 
     ########## 1. Load eval metrics ##########
     dfs = {}
     for external_rep_mode in list(external_rep_modes):
-        df = pd.read_csv(f"/home/jupyter/YD/ZeoPrecLLM/generation_analysis/{model_type}_mode{external_rep_mode}/eval_metrics_summary.csv", index=False)
+        df = pd.read_csv(f"/home/jupyter/YD/ZeoPrecLLM/generation_analysis/{model_type}_mode{external_rep_mode}/eval_metrics_summary.csv")
         dfs[external_rep_mode] = df
 
 
@@ -42,6 +36,7 @@ if __name__ == '__main__':
             plt.plot(df_val["epoch"], df_val[metric], label="val")
             plt.plot(df_test["epoch"], df_test[metric], label="test")
             plt.legend()
+            plt.title(f"{model_type}_mode{external_rep_mode}_{metric}")
             plt.savefig(f"/home/jupyter/YD/ZeoPrecLLM/generation_analysis/{model_type}_mode{external_rep_mode}/eval_metrics_{metric}.jpg")
             plt.show()
     
@@ -61,5 +56,6 @@ if __name__ == '__main__':
                 plt.plot(df_split["epoch"], df_split[metric], label=f"mode{external_rep_mode}")
 
             plt.legend()
+            plt.title(f"{model_type}_{split}_{metric}")
             plt.savefig(save_path + f"{split}_{metric}.jpg")
             plt.show()
